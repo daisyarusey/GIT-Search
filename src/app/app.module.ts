6b96910edb818f673103793a58e154c18fd90fd2
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-23 16:33:37 +0300

    Add class text-center

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearcFormComponent } from './searc-form/searc-form.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearcFormComponent
+    SearcFormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
