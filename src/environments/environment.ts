ed356e72f556e86f3589272f8fc375acdcb40b73
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-24 08:58:15 +0300

    Add api key to enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  GITAPIKEY: "02e77987f33c46e4978ff07d3d210da9f0ed55b6",
 };
 
 /*
