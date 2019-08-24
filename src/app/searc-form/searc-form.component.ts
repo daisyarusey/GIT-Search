a9c7b3c19be6e02c5875af7e89a72be298a29e33
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-24 07:13:23 +0300

    Bind the Input field to a variable in the component

--- src/app/searc-form/searc-form.component.ts
+++ src/app/searc-form/searc-form.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit,EventEmitter } from '@angular/core';
 })
 export class SearcFormComponent implements OnInit {
 
+  searchTerm:string;
   constructor() { }
 
   ngOnInit() {
