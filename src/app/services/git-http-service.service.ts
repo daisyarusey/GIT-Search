7d3d8c40d5de66e683591d3287bb8ea184832d3a
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-24 21:45:02 +0300

    Make API request using promise

--- src/app/services/git-http-service.service.ts
+++ src/app/services/git-http-service.service.ts
@@ -1,13 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitHttpServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   searchUsers(searchTerm:string){
     console.log(searchTerm);
-  }
+    let searchEndpoint = "https://api.github.com/users/daisyarusey?api_key=" + environment.GITAPIKEY;
+    searchEndpoint += "&q="+searchTerm;
+    let promise =  new Promise((resolve, reject)=>{
+      this.http.get(searchEndpoint).toPromise().then(
+        (results)=>{
+          console.log(results);
+          resolve()
+        },
+        (error)=>{
+          console.log(error)
+          reject()
+        }
+      )
+  })
+  return promise;
+}
 }
