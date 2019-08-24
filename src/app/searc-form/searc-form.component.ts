f17e2533653f097bf3d16bbde87e5389d69331a0
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-24 07:55:07 +0300

    Add event emitter

--- src/app/searc-form/searc-form.component.ts
+++ src/app/searc-form/searc-form.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit,EventEmitter } from '@angular/core';
+import { Component, OnInit,EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-searc-form',
   templateUrl: './searc-form.component.html',
   styleUrls: ['./searc-form.component.css']
 })
+
 export class SearcFormComponent implements OnInit {
+
+@Output() emitSearch = new EventEmitter<any>()
 
   searchTerm:string;
   constructor() { }
+
+  search(){
+    this.emitSearch.emit(this.searchTerm);
+  }
 
   ngOnInit() {
   }
