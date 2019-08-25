38b1ea67b25d09564148f14746c91b0ab7db8411
daisyarusey <daisyarusey.dc@gmail.com>
2019-08-26 02:40:47 +0300

    Style class btn-success

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {RepositoryComponent} from './repository/repository.component';
 import {NotFoundComponent} from './not-found/not-found.component';
-import { from } from 'rxjs';
 
 
 const routes: Routes = [
   {"path":"home","component":HomeComponent},
-  {"path":"repository","component":RepositoryComponent},
   {"pathMatch":"full","redirectTo":"home","path":""},
   {"path":"**","component":NotFoundComponent},
 ];
